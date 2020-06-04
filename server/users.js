912fadc95138d2b08e7c5e440d3c581556617cb1
L-Hejer <laouani.h@gmail.com>
2020-06-04 13:24:50 +0100

    endpoint

--- server/users.js
+++ server/users.js
@@ -1,7 +1,7 @@
 const users = [];
 
 const addUser = ({ id, name, room }) => {
-  //Hejer Laouani => hejerlaouani
+  //Hejer Laouani => hejer laouani
   //Room 108 => room108
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
@@ -9,9 +9,8 @@ const addUser = ({ id, name, room }) => {
   const existingUser = users.find(
     (user) => user.room === room && user.name === name
   );
-  if(!name || !room) return { error: 'Username and room are required.' };
+  if (!name || !room) return { error: 'Username and room are required.' };
   if (existingUser) return { error: 'Username is already taken!' };
-  
 
   const user = { id, name, room };
   users.push(user);
